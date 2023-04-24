e3545f49eb5b2338ff651ad77c609a5ed8bd6a59
vickouma77 <Vickprestone20@gmail.com>
2023-04-24 18:04:51 +0300

    exporting data in csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script to export data in the CSV format"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user_name = user.get("username")
+    employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+
+    with open("{}.csv".format(user_id), "w", newline="") as f:
+        writer = csv.writer(f)
